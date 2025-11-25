74f0ef1ca8625fea84e2c9b66b6f558b9dae1441
JoseGildenilson <josegildenilson@gmail.com>
2025-11-25 09:52:02 -0300

    Correção classe Barbeiro
    Ajustei o construtor, corrigi o nome das varíveis e o método exibirDados() para imprimir a comissão e a especialidade

--- src/barbershop/model/Barbeiro.java
+++ src/barbershop/model/Barbeiro.java
@@ -3,15 +3,17 @@ package barbershop.model;
 import barbershop.model.abstratas.Pessoa;
 
 public class Barbeiro extends Pessoa{
-    private String especialiadade;
-    private double comisao;
+    private String especialidade;
+    private double comissao;
 
     //Construtor
-    public Barbeiro(String nome, String cpf, String telefone){
+    public Barbeiro(String nome, String cpf, String telefone, String especialidade, double comissao){
         super(nome, cpf, telefone);
+        this.especialidade = especialidade;
+        this.comissao = comissao;
     }
 
     public void exibirDados(){
-        System.out.println("Nome: " + getNome() + "/CPF: " + getCpf() + "/Telefone: " + getTelefone() + "/Especialidade: " + especialiadade);
+        System.out.println("Nome: " + getNome() + "/CPF: " + getCpf() + "/Telefone: " + getTelefone() + "/Especialidade: " + especialidade + "/ Comissão: " + comissao);
     }
-}
+}   
